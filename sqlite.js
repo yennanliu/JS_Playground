7740662725b29e4366660b44b722b4588e11d6be
yennanliue <f339339@gmail.com>
2017-10-30 15:40:32 +0000

    create simple test db

--- sqlite.js
+++ sqlite.js
@@ -5,22 +5,29 @@
 // install sqlite3 for js 
 // npm install sqlite3 
 
-// -----------------
-
+// import sqlite library 
 const sqlite3 = require('sqlite3').verbose();
  
-// open database in memory
-let db = new sqlite3.Database(':memory:', (err) => {
-  if (err) {
-    return console.error(err.message);
+// create a db in memory 
+//var db = new sqlite3.Database(':memory:');
+// create a db as new file 
+var db = new sqlite3.Database('test.db');
+
+
+// create a new db with table name lorem
+// and run query 
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
   }
-  console.log('Connected to the in-memory SQlite database.');
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
 });
- 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+
+db.close();
