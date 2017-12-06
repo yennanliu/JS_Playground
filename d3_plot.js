eed5fc6da500cf496ba688fb8d79753cd298a649
yennanliue <f339339@gmail.com>
2017-12-06 12:04:22 +0000

    backup d3 plotting script

---
+++ d3_plot.js
@@ -0,0 +1,35 @@
+// please install d3 for node via command below first if you don't have d3 locally 
+// npm install d3
+
+
+// var d3 = require("/lib/d3.js");
+var d3 = require("d3");
+
+//console.log(d3)
+
+
+// d3 scripts 
+//----------------------------------
+
+
+function draw(data) {
+"use strict";
+d3.select("body")
+.append("ul")
+.selectAll("li")
+.data(data)
+.enter()
+.append("li")
+.text(function (d) {
+return d.name + ": " + d.status;
+});
+}
+
+
+//----------------------------------
+
+
+
+
+
+
