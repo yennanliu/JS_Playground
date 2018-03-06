08e348d73995af779b1c1884ee4f2b4f934e06fb
yennanliu <f339339@gmail.com>
2018-03-06 18:55:50 +0000

    backup get object length JS code

---
+++ object_length.js
@@ -0,0 +1,12 @@
+
+
+
+// length of list 
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.length;
+
+
+// length of dict 
+// Object.keys(map.data.b.b).length
+var x = {'a':1,'b':2}
+Object.keys(x).length
