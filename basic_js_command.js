fc09a628b39c537ccc27d790380bfeaaba5469ba
yennanliue <f339339@gmail.com>
2018-10-07 15:08:10 +0100

    add if elseif if example

--- basic_js_command.js
+++ basic_js_command.js
@@ -65,6 +65,22 @@ console.log(typeof  newNumber)
 
 
 
+//----------------------------
+// if , else if, else if, else 
+let stopLight = 'yellow';
+
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down.');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
+
+
+
 
 //----------------------------
 // import library 
