e9f9e9a1f24206a08e695b485464c7e80883015e
yennanliue <f339339@gmail.com>
2018-11-29 22:34:49 +0000

    add simple js demo and its  html

---
+++ basic.js
@@ -0,0 +1,26 @@
+// define func 1 
+function greet(name){
+	console.log('hola welcome ! ' + ' ' + name + '!')
+}
+
+// define func 2 
+function greet_full(first_name, last_name){
+	console.log('nice to see u again !  ' + ' ' + first_name + '-' + last_name  + '!')
+}
+
+// define func 3  : return 
+function sqaure(number){
+	return number*number 
+}
+
+
+// call the func 
+greet('Lin');
+greet_full('Jeremy', 'Lin');
+// 2 ways to get the func return value 
+let x = sqaure(5); 
+console.log(sqaure(5)); 
+
+
+
+
