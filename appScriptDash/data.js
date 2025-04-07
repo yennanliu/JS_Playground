b96c32bc38bc3f8d611b7c849962cbace94f3962
yennj12 <f339339@uber.com>
2025-04-07 16:36:17 +0800

    update

---
+++ appScriptDash/data.js
@@ -0,0 +1,10 @@
+function getData() {
+    return [
+        ['Category', 'Value', 'Value Plus One'],
+        ['A', Math.random() * 50, (Math.random() * 50) + 1],
+        ['B', Math.random() * 50, (Math.random() * 50) + 1],
+        ['C', Math.random() * 50, (Math.random() * 50) + 1],
+        ['D', Math.random() * 50, (Math.random() * 50) + 1],
+        ['E', Math.random() * 50, (Math.random() * 50) + 1]
+    ];
+}
